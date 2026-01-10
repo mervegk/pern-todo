15278714f8b430e0873c7d510fc6c9d6501a7d3e
mervegk <gekemeke123@gmail.com>
2026-01-10 23:28:34 +0300

    add todo

--- frontend/app/api/todo.ts
+++ frontend/app/api/todo.ts
@@ -1,3 +1,5 @@
 import axios from "axios";
 
-export const getTodos = async () => axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}/todos`).then(res => res.data)
+export const getTodos = async () => axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}/todos`).then(res => res.data);
+
+export const addTodos = async (description: string) => axios.post(`${process.env.NEXT_PUBLIC_BASE_URL}/todos`, { description: description }).then(res => res.data);
--- frontend/app/components/listTodos.tsx
+++ frontend/app/components/listTodos.tsx
@@ -1,37 +1,46 @@
 "use client"
 import React, { useState, useEffect } from 'react'
+import { PiTrash, PiNotePencil, PiPlusCircle } from "react-icons/pi";
 import { Todo } from '../types/todo'
 import { getTodos } from '../api/todo';
+import AddTodo from './addTodo';
 
 
 export default function ListTodos() {
   const [todos, setTodos] = useState<Todo[]>([]);
+  const [showAdd, setShowAdd] = useState<boolean>(false)
 
   useEffect(() => {
     const fetchTodos = async () => {
       try {
-        const data = await getTodos()
-        setTodos(data)
+        const data = await getTodos();
+        setTodos(data);
       } catch (err) {
-        console.error("An error occured while fetching todos", err)
+        console.error("An error occured while fetching todos", err);
       }
     }
     fetchTodos();
   }, [])
-  console.log(todos);
-
 
   return (
-    <section className='container mx-auto h-full flex items-center justify-center'>
+    <section className='container mx-auto h-full flex flex-col justify-center relative'>
+      <div className='mb-4 flex self-end'>
+        <button onClick={() => setShowAdd(prev => !prev)} className='text-3xl cursor-pointer'><PiPlusCircle /></button>
+        {showAdd && <AddTodo close={() => setShowAdd(false)} />}
 
+      </div>
       <ul className='w-full grid grid-cols-1 gap-4'>
         {
           todos.map((i, index) => (
-            <li key={i.todo_id} className='grid grid-cols-[70%_30%] items-center w-full p-4 border-2 border-white rounded-lg'>
+            <li key={i.todo_id} className='grid grid-cols-[10%_70%_20%] items-center w-full p-4 border-2 border-white rounded-lg'>
+              <div className='mr-2'>{index + 1}</div>
               <div>
                 {i.description}
               </div>
-              <div className='justify-self-end'>Operations</div>
+              <div className='justify-self-end grid grid-cols-2 items-center gap-1'>
+                <button className='cursor-pointer'><PiNotePencil /></button>
+                <button className='cursor-pointer'><PiTrash /></button>
+              </div>
             </li>
           ))
         }
